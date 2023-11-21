3fffe834a81ef4b84d5eb4ba657b021b7dec0a81
John <john-alex131@hotmail.com>
2023-11-21 17:50:36 -0500

    Subiendo Proyecto

---
+++ lib/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def inicio(request):
+    return HttpResponse("<h1> Bienvenido </h1>")
+
+
+def home(request):
+    return render(request, 'paginas/home.html')
