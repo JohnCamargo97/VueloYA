09393816121d7e2f577421d23180fb9908e02db6
Soy-Mauricio <maurosana6@gmail.com>
2023-12-01 16:23:59 -0500

    database, model & adminpage

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,3 +9,10 @@ class Vuelo(models.Model):
     destino= models.CharField(max_length=100, verbose_name='Destino')
     horasalida= models.DateTimeField(verbose_name='Salida')
     Escalado= models.BooleanField(verbose_name='Escalas')
+
+    def __str__(self):
+        return  self.origen + "-" + self.destino #+ #" " + str(self.horasalida.hour()
+    
+
+
+    
