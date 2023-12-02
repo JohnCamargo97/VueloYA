a6e872bdf62403172f4ec41f4c90bf2190e66200
Soy-Mauricio <maurosana6@gmail.com>
2023-12-02 16:59:32 -0500

    db info transfered to home.html

--- lib/views.py
+++ lib/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+#from django.http import HttpResponse
+from .models import Vuelo
 # Create your views here.
 
 def home(request):
-    return render(request, 'paginas/home.html')
+    lista_vuelos = Vuelo.objects.all()
+    #print(lista_vuelos.id)
+    return render(request, 'paginas/home.html', {'lista_vuelos': lista_vuelos})
 
 def misviajes(request):
     return render (request, 'paginas/misviajes.html')
