fd5f0e7246b4b91698584d4bed823207bf8e4933
John <john-alex131@hotmail.com>
2023-12-12 17:59:38 -0500

    vista para resultados creada, cambio de modelforms a forms, y busqueda funcional.

--- lib/views.py
+++ lib/views.py
@@ -12,13 +12,36 @@ def home(request):
     return render(request, 'paginas/home.html', {'lista_vuelos': lista_vuelos, 'form_busqueda': form_busqueda})
 
 def home_dev2(request):
-    form_busqueda = BusquedaForm(request.POST or None)
-    if form_busqueda.is_valid():
-        print(request.POST)
-        return redirect('misviajes')
+    
+    if request.method == "POST":
+
+        # if this is a POST request we need to process the form data
+        form_busqueda = BusquedaForm(request.POST)
+        
+        if form_busqueda.is_valid():
+                     
+            request.session['origen'] = request.POST['origen']
+            request.session['destino'] = request.POST['destino']
+            print("session updated")
+
+            return redirect('resultados')
+        else:
+            print("campos no validos")
+    else:
+        form_busqueda = BusquedaForm()
+
+ 
     lista_vuelos = Vuelo.objects.all()
-    #print(form_busqueda)
     return render(request, 'paginas/home_dev2.html', {'lista_vuelos': lista_vuelos, 'form_busqueda': form_busqueda})
+
+
+def resultados(request):
+
+    form_response = request.session
+    lista_resultado = Vuelo.objects.filter(origen= form_response['origen']).values()
+    #print(form_response)
+    return render (request, 'paginas/resultados.html', {'lista_resultado': lista_resultado, 'form_response': form_response})
+
 
 def misviajes(request):
     return render (request, 'paginas/misviajes.html')
