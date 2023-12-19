ee4b17b51d12ba2ae48c636fc92229db76ee4f04
John <john-alex131@hotmail.com>
2023-12-19 14:11:29 -0500

    Progreso registros

--- users/views.py
+++ users/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 #from django.contrib.auth.forms import UserCreationForm
 from .forms import formRegistro
+from .models import user
 
 def register(request):
     
     if request.method == "POST":
         form = formRegistro(request.POST or None)
         if form.is_valid():
-            print(request.POST)
-            form.save()
-            return redirect('mensaje_user')
+              
+            usuario_repetido = user.objects.filter(email = request.POST['email']).values()
+            if usuario_repetido:
+                messages.error(request, ('Este usuario ya existe'))
+            else:
+                request.session['email'] = request.POST['email']
+                request.session['fullname'] = request.POST['fullname']
+                print(request.POST)
+                form.save()
+                return redirect('mensaje_user')
         
     else:
         form = formRegistro()
